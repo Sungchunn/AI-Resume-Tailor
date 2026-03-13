922e015dce116745c72c5a9d2cd77c8a92536b64
Sungchunn <99805001+Sungchunn@users.noreply.github.com>
2026-03-13 15:04:58 +0700

    backend: export VersionConflictError from crud.mongo
    - Add VersionConflictError to module exports for easier imports

--- backend/app/crud/mongo/__init__.py
+++ backend/app/crud/mongo/__init__.py
@@ -1,10 +1,12 @@
 """MongoDB CRUD operations."""
 
+from app.crud.mongo.exceptions import VersionConflictError
 from app.crud.mongo.resume import resume_crud, ResumeCRUD
 from app.crud.mongo.tailored_resume import tailored_resume_crud, TailoredResumeCRUD
 from app.crud.mongo.resume_build import resume_build_crud, ResumeBuildCRUD
 
 __all__ = [
+    "VersionConflictError",
     "resume_crud",
     "ResumeCRUD",
     "tailored_resume_crud",
